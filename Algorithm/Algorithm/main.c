88bacac739063bbf44833988bdc6c1cd61fa2682
jiangxintong <jiangxintong@dhc.com.cn>
2020-05-21 16:02:59 +0800

    add a C project about Algorithm

---
+++ Algorithm/Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by JXT on 2020/5/21.
+//  Copyright © 2020 JXT. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
