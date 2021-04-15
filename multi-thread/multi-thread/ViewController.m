0de2d190e7747b5f95f7768ab6dc63f3d6386ed8
MickyChiang <>
2021-04-16 00:45:24 +0800

    update multi-thread

--- multi-thread/multi-thread/ViewController.m
+++ multi-thread/multi-thread/ViewController.m
@@ -18,13 +18,14 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-//    [self interview1]; // 123
-//    [self interview2]; // 132
-//    [self interview3]; // 132
-//    [self interview4]; // 123
-//    [self interview5]; // 12
+//    [self interview1];
+//    [self interview1Change];
+//    [self interview2];
+//    [self interview3];
+//    [self interview4];
+//    [self interview5];
     
-    [self gcd_memo];
+//    [self gcd_memo];
 }
 
 // MARK: - ********************** GCD Memo **********************
@@ -72,28 +73,54 @@
 //    [gcdMemo dispatch_after];
 //    // 单例
 //    [gcdMemo dispatch_once_use];
-    // 信号量
+//    // 信号量
 //    [gcdMemo dispatch_semaphore];
 }
 
+// MARK: - interview multi-thread
+- (void)printLog {
+    NSLog(@"2---%@", [NSThread currentThread]);
+}
 
-
-
-
-
-
-// MARK: - ******************************************
+/*
+ 2021-04-16 00:35:19.428340+0800 multi-thread[8675:210363] 1---<NSThread: 0x6000030e4f00>{number = 4, name = (null)}
+ 2021-04-16 00:35:19.428667+0800 multi-thread[8675:210363] 3---<NSThread: 0x6000030e4f00>{number = 4, name = (null)}
+ 
+ 分析：
+ 由于异步函数dispatch_async是开启一个新的子线程去执行任务，而子线程默认是没有启动Runloop的，所以并不会执行`printLog`方法。
+ 我们可以手动启动runLoop来确保`printLog`被调用，也就是在block里面添加一行代码`[[NSRunLoop currentRunLoop] run];`。
+ */
 - (void)interview1 {
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
         NSLog(@"1---%@", [NSThread currentThread]);
         [self performSelector:@selector(printLog)
                    withObject:nil
                    afterDelay:0]; // performSelector:withObject:afterDelay:的本质是往Runloop中添加定时器
-        [[NSRunLoop currentRunLoop] run]; // 保证performSelector:withObject:afterDelay:方法有效执行
         NSLog(@"3---%@", [NSThread currentThread]);
     });
 }
 
+/*
+ 2021-04-16 00:30:44.055849+0800 multi-thread[8293:204597] 1---<NSThread: 0x6000002e55c0>{number = 7, name = (null)}
+ 2021-04-16 00:30:44.056171+0800 multi-thread[8293:204597] 2---<NSThread: 0x6000002e55c0>{number = 7, name = (null)}
+ 2021-04-16 00:30:44.056364+0800 multi-thread[8293:204597] 3---<NSThread: 0x6000002e55c0>{number = 7, name = (null)}
+ */
+- (void)interview1Change {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSLog(@"1---%@", [NSThread currentThread]);
+        [self performSelector:@selector(printLog)
+                   withObject:nil
+                   afterDelay:0]; // performSelector:withObject:afterDelay:的本质是往Runloop中添加定时器
+         [[NSRunLoop currentRunLoop] run]; // 手动启动runLoop 保证performSelector:withObject:afterDelay:方法有效执行
+        NSLog(@"3---%@", [NSThread currentThread]);
+    });
+}
+
+/*
+ 2021-04-16 00:31:36.586932+0800 multi-thread[8315:205455] 1---<NSThread: 0x600002f640c0>{number = 1, name = main}
+ 2021-04-16 00:31:36.587213+0800 multi-thread[8315:205455] 3---<NSThread: 0x600002f640c0>{number = 1, name = main}
+ 2021-04-16 00:31:36.587752+0800 multi-thread[8315:205455] 2---<NSThread: 0x600002f640c0>{number = 1, name = main}
+ */
 - (void)interview2 {
     dispatch_async(dispatch_get_main_queue(), ^{
         NSLog(@"1---%@", [NSThread currentThread]);
@@ -104,6 +131,15 @@
     });
 }
 
+/*
+ 2021-04-16 00:32:35.513359+0800 multi-thread[8488:206675] 1---<NSThread: 0x6000006449c0>{number = 1, name = main}
+ 2021-04-16 00:32:35.513869+0800 multi-thread[8488:206675] 3---<NSThread: 0x6000006449c0>{number = 1, name = main}
+ 2021-04-16 00:32:35.547090+0800 multi-thread[8488:206675] 2---<NSThread: 0x6000006449c0>{number = 1, name = main}
+ 
+ 分析：
+ 同步函数添加的任务是在当前线程中执行，当前线程就是主线程，而主线程的runLoop是启动的，所以`printLog`会调用。
+ 虽然延迟时间时0秒，但是添加到runLoop中的计时器不是立马触发的，而是要先唤醒runLoop，这是需要消耗一定时间的，所以会先打印3再打印2。
+ */
 - (void)interview3 {
     dispatch_sync(dispatch_get_global_queue(0, 0), ^{
         NSLog(@"1---%@", [NSThread currentThread]);
@@ -114,6 +150,17 @@
     });
 }
 
+/*
+ 2021-04-16 00:33:03.459458+0800 multi-thread[8518:207643] 1---<NSThread: 0x60000032c080>{number = 6, name = (null)}
+ 2021-04-16 00:33:03.459673+0800 multi-thread[8518:207643] 2---<NSThread: 0x60000032c080>{number = 6, name = (null)}
+ 2021-04-16 00:33:03.459836+0800 multi-thread[8518:207643] 3---<NSThread: 0x60000032c080>{number = 6, name = (null)}
+ 
+ 分析：
+ `performSelector:withObject:`函数是不涉及到计时器的，所以不会添加到runLoop中，所以是按照1、2、3的顺序执行。
+ 
+ 注意：
+ `performSelector`系列方法中只要是方法名中包含`afterDelay`、`waitUntilDone`的都是和计时器有关的，都要注意前面出现的这些问题。
+ */
 - (void)interview4 {
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
         NSLog(@"1---%@", [NSThread currentThread]);
@@ -123,7 +170,10 @@
     });
 }
 
-// MARK: - ******************************************
+/*
+ 2021-04-16 00:33:37.341412+0800 multi-thread[8632:208621] 1---<NSThread: 0x600002285a80>{number = 7, name = (null)}
+ 2021-04-16 00:33:37.341734+0800 multi-thread[8632:208621] 2---<NSThread: 0x600002285a80>{number = 7, name = (null)}
+ */
 - (void)interview5 {
     NSThread *thread = [[NSThread alloc] initWithBlock:^{
         NSLog(@"1---%@",[NSThread currentThread]);
@@ -137,10 +187,6 @@
     [thread start];
     
     [self performSelector:@selector(printLog) onThread:thread withObject:nil waitUntilDone:YES];
-}
-
-- (void)printLog {
-    NSLog(@"2---%@", [NSThread currentThread]);
 }
 
 // NSThread + RunLoop 实现常驻线程
